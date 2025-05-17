8f1650fc657a31fb4ad4db05cd15ebdfa6dc60c4
olivia <liv@olivia.cx>
2025-05-16 19:41:34 -0700

    nav.php - remove $sv variable

--- assets/php/nav.php
+++ assets/php/nav.php
@@ -9,15 +9,14 @@
 	<?php $url = ($rt) ? $url.'&rt=1' : $url;?>
 	<?php echo ($sv == 'bhop') ? '<a href="'.$url.'">Server - Bhop</a>' : '<a href="'.$url.'">Server - Climb</a>'; ?>
 	<hr>
-	<a href="index.php?sv=<?=$sv?>">Top Players</a>
-	<a href="index.php?sv=<?=$sv?>&rr=1">Recent Records</a>
-	<a href="index.php?sv=<?=$sv?>&rt=1">Recent Times</a>
+	<a href="index.php">Top Players</a>
+	<a href="index.php?rr=1">Recent Records</a>
+	<a href="index.php?rt=1">Recent Times</a>
 	<hr>
 	<br>
 	Go to map:
 	<br><br>
 	<form action="index.php" method="GET">
-		<input type="hidden" name="sv" id="sv" value="<?php echo $sv;?>" />
 		<select style="max-width: 80%;" name="m" class="form-control" onchange="this.form.submit()" required>
 			<option value="" selected="selected">None</option>
 <?php
@@ -42,7 +41,6 @@
 	(SteamID or Username)
 	<br><br>
 	<form id="search" method="GET">
-		<input type="hidden" name="sv" id="sv" value="<?php echo $sv;?>" />
 		<input type="text" name="u" aria-label="..." placeholder="SteamID or Username" value="<?=(isset($u))?$u:''?>"/>
 			<button type="submit" class="btn btn-primary">Search</button>
 	</form>
